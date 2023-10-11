5f37155a687489d157e103cd6d8393bb32c8074c
PainTrainee <vtec8197@gmail.com>
2023-10-11 14:10:48 +0700

    out Parameter

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,42 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Global ใช้ทุกที่
-string name = "Coffee";
-string id = "11";
-double price = 10;
-double amount = 10;
+string Name = "Coffee";
+string Id = "11";
+double Price = 10;
+double Amount = 10;
 double rate1 = 0.5;
 Random rnd = new();
 //เรียกใช้
 //Input();
-Display();
-Update(ref id, ref name, ref price);
-Display();
+//Display();
+//Update(ref id, ref name, ref price);
+//Display();
 
+rate1 = Cal(out string id, out string name, out double price, out double amount);
+Id = id;
+Name = name;
+Price = price;
+Amount = amount;
+Console.WriteLine($"{Id} {Name} {Price} {Amount}");
+Display();
 //void = no return value
 void Display()
 {
-    Console.WriteLine($"ID : {id}");
-    Console.WriteLine($"Name : {name}");
-    Console.WriteLine($"Price : {price} Amount : {amount} Sum : {price * amount} Process : {Process(rate1)}");
-
+    Console.WriteLine($"ID : {Id}");
+    Console.WriteLine($"Name : {Name}");
+    Console.WriteLine($"Price : {Price} Amount : {Amount} Sum : {Price * Amount} Process : {Process(rate1)}");
 }
 void Input()
 {
     Console.Write("ID : ");
-    id = Console.ReadLine();
+    Id = Console.ReadLine();
     Console.Write("Name : ");
-    name = Console.ReadLine();
+    Name = Console.ReadLine();
     Console.Write("Discount rate 0-1: ");
     rate1 = double.Parse(Console.ReadLine());
-    price = rnd.Next(10, 100);
-    amount = rnd.Next(1, 10);
+    Price = rnd.Next(10, 100);
+    Amount = rnd.Next(1, 10);
 
 }
 double Process(double rate)
 {
     //local เฉพาะที่
-    var sum = price * amount;
+    var sum = Price * Amount;
     var result = sum - (sum * rate);
     return result;
 }
@@ -46,4 +52,13 @@ void Update(ref string Id, ref string Name, ref double Price)
     Id += "555";
     Name += "555";
     Price += 555;
+}
+//out พารามิเตอร์แบบส่งออกอย่างเดียว
+double Cal(out string id,out string name, out double price, out double amount)
+{
+    id = "777";
+    name = "Banana";
+    price = 5;
+    amount = 5;
+    return 0.1;
 }
