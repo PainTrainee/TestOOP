c18a95c58f88f1a5b192295c4214c144951e044d
PainTrainee <vtec8197@gmail.com>
2023-10-23 16:23:40 +0700

    Exercise01_P82T5 (Class, Constructor, Static)

---
+++ Exercise01_P82T5/GenerateScore.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise01_P82T5
+{
+    public class GenerateScore
+    {
+        static public List<Score> ScoreList;
+        public GenerateScore(int number)
+        {
+            ScoreList = new List<Score>();
+            Generate(number);
+        }
+        void Generate(int number) 
+        {
+            Random rnd = new();
+            for (int i = 0; i < number; i++) 
+            {
+                ScoreList.Add(new Score
+                {
+                    Test1 = rnd.Next(0, 100) + 1,
+                    Test2 = rnd.Next(0, 100) + 1,
+                    Test3 = rnd.Next(0, 100) + 1,
+                    Test4 = rnd.Next(0, 100) + 1,
+                });
+            }
+            ScoreList.Max(p=>p.Test1);
+        }
+
+    }
+}
---
+++ Exercise01_P82T5/Program.cs
@@ -0,0 +1,3 @@
+﻿using Exercise01_P82T5;
+var test = new Display();
+test.ShowScore();
