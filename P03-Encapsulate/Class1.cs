3c920ac8dcfb4ac1ae0c8d4712abf4f32dc3232a
PainTrainee <vtec8197@gmail.com>
2023-11-01 10:22:51 +0700

    Using Internal in Project

---
+++ P03-Encapsulate/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace P03_Encapsulate
+{
+    //การเรียกใช้ internal ในโปรเจคเดียวกันสามารถทำได้
+    public class Class1
+    {
+        //Constructor
+        public Class1()
+        {
+            var temp = new BankAccount();
+            temp.Rate = 100;
+        }
+    }
+}
