a61069a1081cffdaef3e39aca82b76cf5b586ec6
PainTrainee <vtec8197@gmail.com>
2023-11-01 11:53:07 +0700

    Inheritance : Using Protected

---
+++ P03-Encapsulate/NewAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace P03_Encapsulate
+{
+    internal class NewAccount : BankAccount
+    {
+        public NewAccount()
+        {
+            Id = 1;
+            Name = "Test";
+            //private Password สืบทอดไม่ได้
+            Rate = 100;
+            //protected สืบทอดได้ แต่เรียกใช้ทางอ้อม
+            Money = 100;
+        }
+        //เรียกใช้ protected แบบทางอ้อม(ภายในคลาส)
+        public void Withdraw(int money)
+        {
+            if(Money > 0 && money <= Money)
+            {
+                Money -= money;
+                Console.WriteLine(Money);
+            }
+            else
+            {
+                Console.WriteLine("Can't withdraw");
+            }
+        }
+    }
+}
--- P03-Encapsulate/Program.cs
+++ P03-Encapsulate/Program.cs
@@ -1,8 +1,11 @@
 ﻿using P03_Encapsulate;
 
-var account = new BankAccount();
-//public, internal
-account.Id = 1;
-account.Name = "Test";
-account.Rate = 10;
-account.SetPassword("1234");
+//var account = new BankAccount();
+////public, internal
+//account.Id = 1;
+//account.Name = "Test";
+//account.Rate = 10;
+//account.SetPassword("1234");
+
+var newAccount = new NewAccount();
+newAccount.Withdraw(100);
