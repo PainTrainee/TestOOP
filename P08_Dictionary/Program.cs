1882f1c198e4311dfe5ed516952a855b775576ad
PainTrainee <vtec8197@gmail.com>
2023-11-21 14:29:42 +0700

    Dictionary

---
+++ P08_Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿Dictionary<string,string> Country = new Dictionary<string, string>();
+Dictionary<int, double> UnitOfMoney = new Dictionary<int, double>();
+Country.Add("Kan", "Kanchanaburi");
+Country.Add("CM", "Chiang Mai");
+Country.Add("BKK", "Bangkok");
+UnitOfMoney.Add(1, 1000_000_000);
+UnitOfMoney.Add(2, 2000_000_000);
+UnitOfMoney.Add(3, 3000_000_000);
+UnitOfMoney.Add(4, 4000_000_000);
+UnitOfMoney.Add(5, 5000_000_000);
+
+if (Country.ContainsKey("Kan")) Console.WriteLine(Country["Kan"]);
+foreach (var item in UnitOfMoney)
+{
+    Console.WriteLine($"{item.Key} {item.Value}");
+}
+var total = UnitOfMoney[1] + UnitOfMoney[2];
+UnitOfMoney.Sum(p => p.Value);
